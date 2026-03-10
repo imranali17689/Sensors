bb99d7c34dcc9b56a48d9a59e009707b1c9b3e9d
Khalil Smith <khalil.smith@spartans.ut.edu>
2026-03-10 03:50:45 -0400

    FEAT - Adding access to the events table in supabase

--- app/routes/garageEvent.py
+++ app/routes/garageEvent.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 from app.models.sensorEvent import SensorEvent
 from app.state.counterStore import applyEvent
 from app.state.counterStore import getStatus
-
+from app.database.supabaseClient import getSupabase
 
 
 router = APIRouter()
+
+supabase = getSupabase()
 
 @router.get("/status")
 def callStatus():
@@ -13,5 +15,11 @@ def callStatus():
 
 @router.post("/event")
 def recievesEvent(event: SensorEvent):
+  supabase.table("Events").insert({
+    "side_id": event.side_id,
+    "direction": event.direction,
+    "timestamp": event.timestamp.isoformat()
+  }).execute()
+
   applyEvent(event)
   return getStatus()
